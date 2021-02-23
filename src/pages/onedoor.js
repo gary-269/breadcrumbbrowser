b9c6fbb965da960dc5744e5765af07de1f830532
gary-269 <karanveersingh269@gmail.com>
2021-02-23 17:41:46 -0500

    made changes

--- src/pages/onedoor.js
+++ src/pages/onedoor.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Onedoor = () => {
-  return <div></div>;
+  return <div>
+      <h1>One door Refrigerators</h1>
+  </div>;
 };
 
 export default Onedoor;
--- src/pages/twodoor.js
+++ src/pages/twodoor.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Twodoor() {
     return (
         <div>
-            
+            <h1>Two door Refrigerators</h1>
         </div>
     )
 }
